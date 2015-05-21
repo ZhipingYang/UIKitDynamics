b73fb67c1aec11550fc9c790129ae7198288a4c2
Zhiping Yang <ZhipingYang@users.noreply.github.com>
2015-05-21 14:55:17 +0800

    Update AppDelegate.m

--- DynamicsDemo/AppDelegate.m
+++ DynamicsDemo/AppDelegate.m
@@ -18,9 +18,11 @@
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     self.window = [[UIWindow alloc]initWithFrame:[UIScreen mainScreen].bounds];
     RootViewController *root = [[RootViewController alloc]init];
-    RootViewController *root = [[RootViewController alloc]init];
+    RootViewController *root = [[RootVie    RootViewController *root = [[RootViewController alloc]init];
+wController alloc]init];
     self.window.rootViewController = [[UINavigationController alloc]initWithRootViewController:root];
-    [self.window makeKeyAndVisible];
+    [self.window makeKeyAndVisible];    RootViewController *root = [[RootViewController alloc]init];
+
     self.window.backgroundColor = [UIColor whiteColor];
     return YES;    RootViewController *root = [[RootViewController alloc]init];
 
@@ -29,19 +31,23 @@
 
 - (void)applicationWillResignActive:(UIApplication *)application {
     // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
-    // Use this method to pause ongoing tasks, disable timers, and throttle down OpenGL ES frame
+    // Use this method to pause ongoin    RootViewController *root = [[RootViewController alloc]init];
+g tasks, disable timers, and throttle down OpenGL ES frame
         RootViewController *root = [[RootViewController alloc]init];
 rates. Games should use this method to pause the game.
 }
 
-- (void)applicationDidEnterBackground:(UIApplication *)application {
+- (void)applicationDidEnterBackground:    RootViewController *root = [[RootViewController alloc]init];
+(UIApplication *)application {
     // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later.
     // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
-}
+}    RootViewController *root = [[RootViewController alloc]init];
+
 
 - (void)applicationWillEnterForeground:(UIApplication *)application {
     // Called as part of the transition from the background to the inactive state; here you can undo many of the changes made on entering the background.
-}
+}    RootViewController *root = [[RootViewController alloc]init];
+
 
 - (void)applicationDidBecomeActive:(UIApplication *)application {
     // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
